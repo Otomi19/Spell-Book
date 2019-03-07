0683c614b1027afddc12d7e83a32a47bd87f17ea
Otomi19 <43240483+Otomi19@users.noreply.github.com>
2019-03-07 08:28:54 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,11 @@
+$(".up").click(function(){
+    $(".ik").fadeToggle();
+});
+$(".de").mouseover(function( ){
+    $("#bye").slideToggle( );
+});
+$(".yi").click(function(){
+    $("#heyo").toggle();
+    $(".none").toggle();
+});
+
